cd2546b0674addafb65ef5bbe28772eab154e43d
Shaonkabir <shaonkabir98@gmail.com>
2020-03-30 23:38:20 +0600

    refactor(location): fix logic when location will be off.

--- demo/app.js
+++ demo/app.js
@@ -1,16 +1,27 @@
 
-// grab the geo-location container from DOM
-const latitudePoint = document.querySelector('.info__location--latitude');
-const longitudePoint = document.querySelector('.info__location--longitude');
+
+
+window.onload = function() {
+    // grab the geo-location container from DOM
+    const latitudePoint = document.querySelector('.info__location--latitude');
+    const longitudePoint = document.querySelector('.info__location--longitude');
 
 
 
-// check if geoloaction is available
-if (navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position => {
-        const { longitude, latitude } = position.coords;
-        latitudePoint.innerHTML= `${latitude}`;
-        longitudePoint.innerHTML= `${longitude}`;
-        
-    });
+    // check if geoloaction is available
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            const { longitude, latitude } = position.coords;
+            latitudePoint.innerHTML= `${latitude}`;
+            longitudePoint.innerHTML= `${longitude}`;
+            
+            // hide the warnings for Allowing location.
+            document.querySelector('.info__warning').style.display = 'none';
+        });
+    }
+
+
 }
+
+
+
