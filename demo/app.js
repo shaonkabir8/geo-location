b00366b182383a3e471a00098f2a28030f50d215
Shaonkabir <shaonkabir98@gmail.com>
2020-03-30 23:19:46 +0600

    refactor(location): displayed current geolocation lang, lat value

--- demo/app.js
+++ demo/app.js
@@ -1,3 +1,16 @@
 
+// grab the geo-location container from DOM
+const latitudePoint = document.querySelector('.info__location--latitude');
+const longitudePoint = document.querySelector('.info__location--longitude');
 
-// let's start !!
+
+
+// check if geoloaction is available
+if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position => {
+        const { longitude, latitude } = position.coords;
+        latitudePoint.innerHTML= `${latitude}`;
+        longitudePoint.innerHTML= `${longitude}`;
+        
+    });
+}
